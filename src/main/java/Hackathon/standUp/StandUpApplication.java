7765a7042da4d8db8c22a1e75989ecee2753789a
unknown <kim0615157@gmail.com>
2025-09-05 14:58:07 +0900

    init

---
+++ src/main/java/Hackathon/standUp/StandUpApplication.java
@@ -0,0 +1,13 @@
+package Hackathon.standUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hackathon/standUp/StandUpApplicationTests.java
@@ -0,0 +1,13 @@
+package Hackathon.standUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
