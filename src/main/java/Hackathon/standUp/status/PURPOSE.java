2772a37f9af5449c146c5771562d6da3e64a64f1
unknown <kim0615157@gmail.com>
2025-09-06 00:11:28 +0900

    chore

--- src/main/java/Hackathon/standUp/status/PURPOSE.java
+++ src/main/java/Hackathon/standUp/status/PURPOSE.java
@@ -4,5 +4,5 @@ public enum PURPOSE {
     STORE_INTRODUCTION,     // 가게소개
     NEW_MENU_PRODUCT,       // 신메뉴/제품
     EVENT,                  // 이벤트
-    NOTICE,                 // 공지사항
+    NOTICE                  // 공지사항
 }
