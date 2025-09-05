37124bb8b9168dd6418e4cea65a88aec9235164b
unknown <kim0615157@gmail.com>
2025-09-05 16:27:24 +0900

    feat:클라이언트 -> 서버 -> ai -> 서버 -> 클라이언트

--- src/main/java/Hackathon/standUp/controller/StandUpController.java
+++ src/main/java/Hackathon/standUp/controller/StandUpController.java
@@ -2,6 +2,8 @@ package Hackathon.standUp.controller;
 
 import Hackathon.standUp.dto.request.PromotionRequest;
 import Hackathon.standUp.dto.response.PromotionResponse;
+import Hackathon.standUp.service.ProxyService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +15,18 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/stand-up")
 public class StandUpController {
 
-    public StandUpController() {
+    private final ProxyService proxyService;
 
+    public StandUpController(ProxyService proxyService) {
+        this.proxyService = proxyService;
     }
 
     @PostMapping("/proxy")
     public ResponseEntity<PromotionResponse> proxyPromotion(
         @RequestPart(name = "request") PromotionRequest request,
         @RequestPart(name = "img", required = false) MultipartFile multipartFile) {
+        PromotionResponse response = proxyService.proxyPromotion(request, multipartFile);
 
-
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
